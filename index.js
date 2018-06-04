c7a0899aaa3a080fd5a3b17865e21c2ad162c2b1
aasheesh <aasheesh.bareja@humonicsglobal.com>
2018-06-04 17:50:11 +0530

    Mongoose Uninstall

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
-var express = require('express'), db = require('./Utils/db'),
-app = express();
- 
-
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app is running!
-app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date());
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
 });
 
 
 
-app.listen(8080, ip);
 
 
 
-module.exports = app;
+
+
+
+
+
+// var express = require('express'), db = require('./Utils/db'),
+// app = express();
+ 
+
+// var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+
+// // app is running!
+// app.get('/', function(req, res) {
+//     res.send('Hello from NodeJS  at '+ new Date());
+// });
+
+
+
+// app.listen(8080, ip);
+
+
+
+// module.exports = app;
