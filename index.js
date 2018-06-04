8ff3f6d35b1d2e15327b23d011e20354cdbaa453
aasheesh <aasheesh.bareja@humonicsglobal.com>
2018-06-04 18:11:41 +0530

    mogo coneection updated code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/humo_db");
+mongoose.createConnection('localhost','humo_db')
 var nameSchema = new mongoose.Schema({
     firstName: String,
     lastName: String
